518ab4eb71fef1692c09b3a5f02f918b48568619
Ygor Mendes <ygortmendes@gmail.com>
2021-01-20 15:54:07 -0300

    create back end

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '07d33c3850ceb45057f3dffb68f00009',
+    expiresIn: '1d',
+  }
+}
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,48 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
+
+interface UserInterface {
+  name: string;
+  email: string;
+  password?: string;
+}
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<UserInterface> {
+
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({ 
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+
+}
+
+export default CreateUserService;
