2118a23e7838190b3f1c5d49101936dd6f864a7f
Ygor Mendes <ygortmendes@gmail.com>
2021-01-31 19:02:57 -0300

    tests in create user

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('Should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUsers = new CreateUserService(
+      fakeUsersRepository
+    );
+
+    const  user = await createUsers.execute({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      password: '123456'
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUsers = new CreateUserService(fakeUsersRepository);
+
+    await createUsers.execute({
+      name: 'John Doe',
+      email: 'john@gmail.com',
+      password: '123456'
+    });
+
+    expect(
+      createUsers.execute({
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        password: '123456'
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
